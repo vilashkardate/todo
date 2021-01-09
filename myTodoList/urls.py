6b532938196e6f43e0dbc91c5f61a562cc9bc050
Vaibhav Kardate <vaibhavkardate@Vilash.local>
2021-01-09 10:44:00 +0530

    Added form a capture date and implemented button functions

--- myTodoList/urls.py
+++ myTodoList/urls.py
@@ -18,6 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('todoListapp.urls')),                                                                                                                                                                                                                                                                                                               
+    path('',include('todolist.urls')),                                                                                                                                                                                                                                                                                                               
 
 ]
--- todoListapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import TodoList
-# Create your views here.
-
-def index(request):
-    todo_items = TodoList.objects.order_by('id')
-    context = {'todo_items' : todo_items}
-    return render(request,'todoListapp/index.html', context)
